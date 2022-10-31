30c67bcedee16967ae9684b6f1cca32815644515
Mary90272 <lefebvre.mary90272@gmail.com>
2022-10-30 21:23:44 -0700

    full work

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # user-password
+
+## Description
+
+
+This week my task was to generate a passport for the user from 8-128 characters. Plus, the user had the right to choose (Uppercase, lowercase, digits and special characters)
+To simplify the task a little, for this I used a checkbox.
+the user has the right to choose the total number of characters and just click the checkbox on your preference ( uppercase lowercase digits and special character)
+We have 5 variables. which in the future * by the number of checkboxes selected or not selected by the user. using  math floor /math random. It's a clear and fast way to generate passports for users.
+I've also added css for a more pleasant look on my page.
+ 
+
+ ## Installation
+ N/A
+
+## Usage
+
+
+
+## Credits 
+N/A
+## License
---
+++ assets/js/script.js
@@ -0,0 +1,109 @@
+// Assignment code here
+var allPasswordChars = [
+  {
+    'label': 'lowercase',
+    'chars': 'abcdefghijklmnopqrstuvxyz'
+  },
+  {
+    'label': 'uppercase',
+    'chars': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  },
+  {
+    'label': 'specials',
+    'chars': '~`! @#$%^&*()_-+={[}]|\:;"\'<,>.?/'
+  },
+  {
+    'label': 'digits',
+    'chars': '0123456789'
+  }
+]
+
+var passwordLengthInp = document.querySelector('#password-length-inp');
+var containsLowercaseInp = document.querySelector('#contains-lowercase-inp');
+var containsUppercaseInp = document.querySelector('#contains-uppercase-inp');
+var containsSpecialCharsInp = document.querySelector('#contains-special-chars-inp');
+var containsDigitsInp = document.querySelector('#contains-digits-inp');
+
+function generatePassword() {
+  var passwordLength = Number(passwordLengthInp.value);
+  var containsLowercase = containsLowercaseInp.checked;
+  var containsUppercase = containsUppercaseInp.checked;
+  var containsSpecialChar = containsSpecialCharsInp.checked;
+  var containsDigits = containsDigitsInp.checked;
+
+  if(!containsLowercase && !containsUppercase && !containsSpecialChar && !containsDigits) {
+    return '';
+  }
+
+  var password = Array(passwordLength).fill(null);
+  var selectedCharSetIdxes = [];
+
+  if(containsLowercase) {
+    selectedCharSetIdxes.push(0);
+    while(true) {
+      var randPasswordIdx = Math.floor(Math.random() * passwordLength);
+      if(password[randPasswordIdx] == null) {
+        password[randPasswordIdx] = allPasswordChars[0]['chars'][Math.floor(Math.random() * allPasswordChars[0]['chars'].length)];
+        break;
+      }
+    }
+  }
+
+  if(containsUppercase) {
+    selectedCharSetIdxes.push(1);
+    while(true) {
+      var randPasswordIdx = Math.floor(Math.random() * passwordLength);
+      if(password[randPasswordIdx] == null) {
+        password[randPasswordIdx] = allPasswordChars[1]['chars'][Math.floor(Math.random() * allPasswordChars[1]['chars'].length)];
+        break;
+      }
+    }
+  }
+
+  if(containsSpecialChar) {
+    selectedCharSetIdxes.push(2);
+    while(true) {
+      var randPasswordIdx = Math.floor(Math.random() * passwordLength);
+      if(password[randPasswordIdx] == null) {
+        password[randPasswordIdx] = allPasswordChars[2]['chars'][Math.floor(Math.random() * allPasswordChars[2]['chars'].length)];
+        break;
+      }
+    }
+  }
+
+  if(containsDigits) {
+    selectedCharSetIdxes.push(3);
+    while(true) {
+      var randPasswordIdx = Math.floor(Math.random() * passwordLength);
+      if(password[randPasswordIdx] == null) {
+        password[randPasswordIdx] = allPasswordChars[3]['chars'][Math.floor(Math.random() * allPasswordChars[3]['chars'].length)];
+        break;
+      }
+    }
+  }
+
+  for(var i = 0; i < password.length; i++) {
+    if(password[i] == null) {
+      var randCharSetIdx = selectedCharSetIdxes[Math.floor(Math.random() * selectedCharSetIdxes.length)];
+      var randChar = allPasswordChars[randCharSetIdx]['chars'][Math.floor(Math.random() * allPasswordChars[randCharSetIdx]['chars'].length)];
+      password[i] = randChar;
+    }
+  }
+
+  return password.join('');
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
